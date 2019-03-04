f51e0f94a48d1535d256f3efe2b7c88dfdb37586
Bob <bob.e.baxter@gmal.com>
2019-03-04 17:16:30 -0600

    working on api get request

--- swgoh.js
+++ swgoh.js
@@ -1,4 +1,4 @@
-const app = document.querySelector("main");
+const app = document.getElementById('charTable');
 // const logo = document.createElement('img');
 // logo.src = ''; //need to fill with logo picture
 const container = document.createElement('div');
@@ -10,13 +10,50 @@ app.appendChild(container);
 let request = new XMLHttpRequest();
 
 // Open a new connection, using the GET request on the URL endpoint
-request.open('GET', 'https://swgoh.gg/api/player/492912899/', true);
+request.open('GET', 'https://cors.io/?https://swgoh.gg/api/player/492912899/', true);
 
 request.onload = function () {
   // Begin accessing JSON data here
   let data = JSON.parse(this.response);
 
   if (request.status >= 200 && request.status < 400) {
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    let character = [];
+
+    let getCharType = function () {
+      data.units.forEach(toon => {
+        // console.log(toon.data.name);
+        // console.log(toon.data.rarity);
+        // console.log(toon.data.gear_level);
+        // console.log(toon.data.level);
+      });
+    }
+
+    getCharType();
+  }
+}
+    
+    // data.units.forEach(toon => {
+    //   character.
+    // })
+    
+//     if (data.toon.data.combat_type = 1) {
+//       data.units.forEach(toon => {
+//         console.log(toon.data.name);
+//         console.log(toon.data.rarity);
+//         console.log(toon.data.gear_level);
+//         console.log(toon.data.level);
+//       });
+//     }
+//   } else {
+//     console.log('error');
+//   }
+// }
+
+  
+
+  /* if (request.status >= 200 && request.status < 400) {
     data.forEach(toon => {
       // Create a div with a charList class
       const table = document.createElement('table');
@@ -40,7 +77,7 @@ request.onload = function () {
   } else {
     console.log('error');
   }
-};
+}; */
 
 // Send request
-request.send();
+request.send ();
